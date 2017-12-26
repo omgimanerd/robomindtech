df38424a20c554c9941eae521833974c4659ec47
omgimanerd <alvin@omgimanerd.tech>
2017-12-26 17:42:23 -0500

    More improvements

--- server.js
+++ server.js
@@ -5,9 +5,12 @@
 
 const PORT = 5000
 
+const PAGES = ['home', 'about', 'programs', 'signup']
+
 // Dependencies.
 const express = require('express')
 const http = require('http')
+const morgan = require('morgan')
 const path = require('path')
 
 const app = express()
@@ -17,10 +20,23 @@ app.set('port', PORT)
 app.set('view engine', 'pug')
 
 // app.use('/favicon.ico', express.static(path.join(__dirname)))
+app.use(morgan('dev'))
 app.use('/client', express.static(path.join(__dirname, '/client')))
 
-app.use('/', (request, response) => {
-  response.render('index')
+app.get('/', (request, response) => {
+  response.redirect('/home')
+})
+
+app.get('/:page', (request, response, next) => {
+  if (PAGES.includes(request.params.page)) {
+    response.render(request.params.page)
+  } else {
+    next()
+  }
+})
+
+app.use((request, response) => {
+  response.status(404).render('404')
 })
 
 // Starts the server.
